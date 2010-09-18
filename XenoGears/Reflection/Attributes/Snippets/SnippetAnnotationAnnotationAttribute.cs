605642d27a00fb7951b5f31aa48523e5ea0aa3dc
xeno.by <xeno.by@gmail.com>
2010-09-19 00:33:58 +0300

    Upgraded snippets

--- XenoGears/Reflection/Attributes/Snippets/SnippetAnnotationAnnotationAttribute.cs
+++ XenoGears/Reflection/Attributes/Snippets/SnippetAnnotationAnnotationAttribute.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Diagnostics;
 using XenoGears.Assertions;
 
 namespace XenoGears.Reflection.Attributes.Snippets
@@ -9,15 +8,14 @@ namespace XenoGears.Reflection.Attributes.Snippets
     // it's an Attribute that Annotates attributes that Annotate snippets of code
 
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
-    [DebuggerNonUserCode]
     public class SnippetAnnotationAnnotationAttribute : Attribute
     {
         public Type AssemblyMarker { get; set; }
         public Type TypeMarker { get; set; }
         // MethodMarker is an attribute that is annotated with this attribute
         // see SnippetsRegistry.For<A>
-//        public Type MethodMarker { get; set; }
-        public bool AutoIncludeAllMethodsInMarkedClasses { get; set; }
+//        public Type MemberMarker { get; set; }
+        public bool AutoIncludeAllMembersInMarkedClasses { get; set; }
 
         private enum WeightAccumulation { Additive, Multiplicative }
         private WeightAccumulation _weightAccumulation;
