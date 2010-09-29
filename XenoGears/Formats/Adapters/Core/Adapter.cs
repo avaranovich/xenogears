8a634712267014948f2a66ed154216796b7eb7cb
xeno.by <xeno.by@gmail.com>
2010-09-29 20:04:58 +0300

    Implementing configuration for JSON serialization/deserialization

--- XenoGears/Formats/Adapters/Core/Adapter.cs
+++ XenoGears/Formats/Adapters/Core/Adapter.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Reflection;
+using XenoGears.Reflection.Attributes.Weight;
 
-namespace XenoGears.Formats.Annotations.Adapters
+namespace XenoGears.Formats.Adapters.Core
 {
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Class | AttributeTargets.Interface, AllowMultiple = false, Inherited = true)]
     [DebuggerNonUserCode]
-    public abstract class Adapter : Attribute
+    public abstract class Adapter : WeightedAttribute
     {
-        public abstract Object AfterDeserialize(MemberInfo mi, Object value);
-        public abstract Object BeforeSerialize(MemberInfo mi, Object value);
+        public virtual Object AfterDeserialize(MemberInfo mi, Object value) { return value; }
+        public virtual Object BeforeSerialize(MemberInfo mi, Object value) { return value; }
     }
 }
