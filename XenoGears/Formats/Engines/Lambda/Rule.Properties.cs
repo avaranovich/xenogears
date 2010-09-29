a7cc8223e4f0b49f49cc61ec35fcfc92136bfc7b
xeno.by <xeno.by@gmail.com>
2010-09-29 20:32:06 +0300

    Implementing configuration for JSON serialization/deserialization

---
+++ XenoGears/Formats/Engines/Lambda/Rule.Properties.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using XenoGears.Formats.Configuration;
+
+namespace XenoGears.Formats.Engines.Lambda
+{
+    public static partial class LambdaEngines
+    {
+        public static PropertyRule Engine<T>(this PropertyRule rule, Func<Json, T> deserialize, Func<T, Json> serialize)
+        {
+            return rule.Record(cfg => cfg.Engine(deserialize, serialize));
+        }
+
+        public static PropertyRule Engine(this PropertyRule rule, Func<PropertyInfo, Json, Object> deserialize, Func<PropertyInfo, Object, Json> serialize)
+        {
+            return rule.Record(cfg => cfg.Engine(deserialize, serialize));
+        }
+
+        public static PropertyRule Engine(this PropertyRule rule, Func<Json, Object> deserialize, Func<PropertyInfo, Object, Json> serialize)
+        {
+            return rule.Record(cfg => cfg.Engine(deserialize, serialize));
+        }
+
+        public static PropertyRule Engine(this PropertyRule rule, Func<PropertyInfo, Json, Object> deserialize, Func<Object, Json> serialize)
+        {
+            return rule.Record(cfg => cfg.Engine(deserialize, serialize));
+        }
+
+        public static PropertyRule Engine(this PropertyRule rule, Func<Json, Object> deserialize, Func<Object, Json> serialize)
+        {
+            return rule.Record(cfg => cfg.Engine(deserialize, serialize));
+        }
+    }
+}
