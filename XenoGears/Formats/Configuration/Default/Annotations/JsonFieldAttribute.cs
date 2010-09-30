8bc22c276d58d2ca3b317dbf6cbbde161215eee1
xeno.by <xeno.by@gmail.com>
2010-09-30 20:43:35 +0300

    Next stab at implementing default engine

---
+++ XenoGears/Formats/Configuration/Default/Annotations/JsonFieldAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace XenoGears.Formats.Configuration.Default.Annotations
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    [DebuggerNonUserCode]
+    public class JsonFieldAttribute : JsonIncludeAttribute
+    {
+        public JsonFieldAttribute()
+        {
+        }
+
+        public JsonFieldAttribute(String name)
+            : base(name)
+        {
+        }
+    }
+}
