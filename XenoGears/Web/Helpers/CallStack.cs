89c30de55b87e1a2450d6270856c49c94a2ab9bb
moss_user <moss_user@localhost>
2010-11-26 09:21:26 +0200

    Minimum log level is now unrelated to DEBUG compilation flag, since we might deal with web apps, which get ruled by Web.config

--- XenoGears/Web/Helpers/CallStack.cs
+++ XenoGears/Web/Helpers/CallStack.cs
@@ -14,11 +14,7 @@ namespace XenoGears.Web.Helpers
                 // todo. this creates a potential security hole!
                 // note. though I had to do this since commented line ain't work
 //                var customErrorsDisabled = !HttpContext.Current.IsCustomErrorEnabled;
-#if DEBUG
-                var customErrorsDisabled = true;
-#else
-                    var customErrorsDisabled = false;
-#endif
+                var customErrorsDisabled = Debug.Disabled;
 
                 return debugEnabled && customErrorsDisabled;
             }
